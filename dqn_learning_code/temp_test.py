60f011c924d15c37adf22ef912742f38ebb7f2ef
Yang Yejun <purunyj@kaist.ac.kr>
2018-11-06 14:49:14 +0900

    test
    test

--- dqn_learning_code/temp_test.py
+++ dqn_learning_code/temp_test.py
@@ -4,3 +4,7 @@ print([[random.random()+0.7,-10*random.random()-20],[-random.random()-0.2,-10*ra
                         [10*random.random()+20,0.5*random.random()+0.4],[-10*random.random()-20,-0.5*random.random()-0.4]])
 print([[random.random()+0.7,10*random.random()+20],[-random.random()-0.2,10*random.random()+20],\
                         [-10*random.random()-20,0.5*random.random()+0.4],[10*random.random()+20,-0.5*random.random()-0.4]])
+print([[random.random()+0.7,10*random.random()+20],[-random.random()-0.2,10*random.random()+20],\
+                        [-10*random.random()-20,0.5*random.random()+0.4],[10*random.random()+20,-0.5*random.random()-0.4]])
+print([[random.random()+0.7,10*random.random()+20],[-random.random()-0.2,10*random.random()+20],\
+                        [-10*random.random()-20,0.5*random.random()+0.4],[10*random.random()+20,-0.5*random.random()-0.4]])
