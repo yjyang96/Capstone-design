53d3e4a31d251e5652887ec9eae3ae48d3677a89
Yang YeJun <purunyj@kaist.ac.kr>
2018-11-29 00:29:28 +0900

    change overall flow, make readmarker by python

--- catkin_ws_NUC/src/readmarker/src/CNN_test.py
+++ catkin_ws_NUC/src/readmarker/src/CNN_test.py
@@ -20,8 +20,6 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 data_T= transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
